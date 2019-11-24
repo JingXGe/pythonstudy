c00b0cf7be9c85b3fb8454c84ada1a65d6bca2d6
gejingyi <915954814@qq.com>
2019-11-25 00:55:17 +0800

    edit

--- test1.py
+++ test1.py
@@ -6,11 +6,17 @@
       注释
     汇率转换器；输入美元显示人民币
 """
-# 1.获取数据
-str_usd = input("请输入美元")
-float_usd = float(str_usd)
-# 2.逻辑处理
-rmb = float_usd * 6.7
+# # 1.获取数据
+# str_usd = input("请输入美元")
+# float_usd = float(str_usd)
+# # 2.逻辑处理
+# rmb = float_usd * 6.7
+#
+# # 3.显示结果
+# print(rmb)
 
-# 3.显示结果
-print(rmb)
+print("qtx")
+qtx = input("context")
+
+print(qtx)
+
